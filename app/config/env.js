8435eb80467fa8beb6216aa96be5a6eec6611096
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-09-15 11:19:42 +0530

    Update env.js

--- app/config/env.js
+++ app/config/env.js
@@ -2,7 +2,7 @@ const env = {
   database: 'loizenaidb',
   username: 'postgres',
   password: '123',
-  host: 'localhost',
+  host: 'postgres-service',
   dialect: 'postgres',
   pool: {
     max: 5,
