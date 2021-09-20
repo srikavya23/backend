d722d67821d5c17c49a342039597779b16f71983
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-09-20 12:30:10 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,7 +3,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
   host: process.env.HOST,
-  dialect: process.env.DIALECT,
+  dialect: 'postgres',
   operatorsAliases: false,
  
   pool: {
