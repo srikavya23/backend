d590d635f08024d755ac71ebe3974f7288597942
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-09-20 12:32:31 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 //const env = require('./env.js');
- 
+const url = { url: process.env.proxy}; 
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
   host: process.env.HOST,
