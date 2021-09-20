2fab42d4e164cbd7431cbff6470d1bd18813b210
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-09-20 11:07:08 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,16 +1,18 @@
 const env = require('./env.js');
 
+const url = { url: process.env.REACT_APP_API_URL}; //backend service name
+
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(env.database, env.username, env.password, {
-  host: env.host,
-  dialect: env.dialect,
+const sequelize = new Sequelize(process.env.database, pocess.env.username, process.env.password, {
+  host: process.env.host,
+  dialect: process.env.dialect,
   operatorsAliases: false,
 
   pool: {
-    max: env.max,
-    min: env.pool.min,
-    acquire: env.pool.acquire,
-    idle: env.pool.idle
+    max: process.env.max,
+    min: process.env.pool.min,
+    acquire: process.env.pool.acquire,
+    idle: process.env.pool.idle
   }
 });
 const db = {};
