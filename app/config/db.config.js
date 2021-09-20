b68ab2f53e30a46ade78de3fa5ef413e5674b896
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-09-20 11:53:19 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,25 +1,23 @@
-const env = require('./env.js');
-
-const url = { url: process.env.proxy}; //backend service name
-
+//const env = require('./env.js');
+ 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.database, pocess.env.username, process.env.password, {
-  host: process.env.host,
-  dialect: process.env.dialect,
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
+  host: process.env.HOST,
+  dialect: process.env.DIALECT,
   operatorsAliases: false,
-
+ 
   pool: {
-    max: process.env.max,
-    min: process.env.pool.min,
-    acquire: process.env.pool.acquire,
-    idle: process.env.pool.idle
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
 });
 const db = {};
-
+ 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-
+ 
 db.Customer = require('../models/customer.model.js')(sequelize, Sequelize);
-
+ 
 module.exports = db;
