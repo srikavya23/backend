4359e726e538ca638a2d1cb54a6c2fcba94672af
srikavya23 <82562508+srikavya23@users.noreply.github.com>
2021-09-21 08:51:23 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,4 @@
 const env = require('./env.js');
-require('dotenv').config();
  
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(env.database, env.username, env.password, {
